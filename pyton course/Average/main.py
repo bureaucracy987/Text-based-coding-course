c2fa0e2d861f939f393fb2eef5df55c646e9c548
bureaucracy987 <tulika.de475@gmail.com>
2025-04-12 09:17:23 +0530

    Average

---
+++ pyton course/Average/main.py
@@ -0,0 +1,9 @@
+tree1=78
+tree2=99
+tree3=32
+tree4=89
+tree5=67
+sum=tree1+tree2+tree3+tree4+tree5
+print("The sum of all the trees is:",sum)
+average=sum/5
+print("The average of all the trees is equal to:",average)
