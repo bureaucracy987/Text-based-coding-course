f6632252b170e4dd7750037300d19180d66ac697
bureaucracy987 <tulika.de475@gmail.com>
2025-04-14 11:44:41 +0530

    Conditional Statements

---
+++ pyton course/Conditional Statements/main.py
@@ -0,0 +1,10 @@
+answer=input("When was the first known use of the word 'quiz'?")
+if answer== "1781":
+    print("Correct!")
+else:
+    print(f"The answer is 1781, not {answer}.")
+answer=input("When did India win its first cricket world cup?")
+if answer== "1983":
+    print("Correct!")
+else:
+    print(f"The answer is 1983, not {answer}.")
