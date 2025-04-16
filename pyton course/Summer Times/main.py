8d2045ebdfc5fbdfd08b37f617d829213cc12e9c
bureaucracy987 <tulika.de475@gmail.com>
2025-04-16 08:57:47 +0530

    Summer Times

---
+++ pyton course/Summer Times/main.py
@@ -0,0 +1,5 @@
+answer=input("Is this winter or summer?")
+if answer=="summer":
+    print("Please wear light cotton clothes")
+else:
+    print("Please consider wearing woollen clothes and pullovers.")
