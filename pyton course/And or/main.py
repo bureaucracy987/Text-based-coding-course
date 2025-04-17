65d5cdfafabb228be6e6c4be5ed2cfd0742c5acb
bureaucracy987 <tulika.de475@gmail.com>
2025-04-17 09:25:10 +0530

    And or

---
+++ pyton course/And or/main.py
@@ -0,0 +1,14 @@
+a=10
+b=12
+c=20
+if a>0 and c>0:
+    print("All the numbers are greater than zero.")
+else:
+    print("Atleast one number is greater than zero.")
+a=10
+b=-10
+c=0
+if a>0 or b>0:
+    print("Either of the number is greater than zero.")
+else:
+    print("None of the numbers are greater than zero.")
