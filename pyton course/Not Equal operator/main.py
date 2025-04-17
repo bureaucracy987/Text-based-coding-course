763d5549b928c7ed2bc8adfca10805469614df89
bureaucracy987 <tulika.de475@gmail.com>
2025-04-17 09:41:37 +0530

    Not Equal

---
+++ pyton course/Not Equal operator/main.py
@@ -0,0 +1,18 @@
+a=10
+b=12
+c=12
+print(a != b)
+print(b != c)
+a="Orange"
+b="mango"
+if a != b:
+    print(a, "and",b, "are different.")
+a=4
+b=5
+if (a==1) != (b==5):
+    print("Hello")
+a=int(input("Enter any number:"))
+if a%2 != 0:
+    print(a, "is an odd number.")
+else:
+    print(a, "is an even number.")
