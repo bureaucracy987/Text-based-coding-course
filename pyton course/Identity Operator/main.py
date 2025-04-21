c4b9b07f61fa5a36bea47cea3995d4274faa90f9
bureaucracy987 <tulika.de475@gmail.com>
2025-04-21 11:01:36 +0530

    Identity Operator

---
+++ pyton course/Identity Operator/main.py
@@ -0,0 +1,12 @@
+a="Debarjya"
+b="hello world"
+if(a in b):
+ print("True")
+if(a not in b):
+ print("False")
+a="hello"
+b="hello world"
+if(a in b):
+ print("True")
+if(a not in b):
+ print("False")
