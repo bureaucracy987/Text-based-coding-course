b92a1f384eb4c97d6ab07b9e6603de43ae86dc20
bureaucracy987 <tulika.de475@gmail.com>
2025-04-24 09:33:52 +0530

    Mean Value

---
+++ pyton course/Mean Value/main.py
@@ -0,0 +1,10 @@
+mean1=38
+wrong_number=36
+correct_number=56
+total_number=40
+sum=mean1*total_number
+print("The sum of 40 numbers is",sum)
+sum2=sum-wrong_number+correct_number
+print("The corrected sum is",sum2)
+mean2=sum2/total_number
+print(mean2)
