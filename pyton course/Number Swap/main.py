f61ffac84f2c9e0da194e47bb536a92d420fb693
bureaucracy987 <tulika.de475@gmail.com>
2025-04-28 11:10:26 +0530

    Exam Eligibility Check

---
+++ pyton course/Number Swap/main.py
@@ -0,0 +1,8 @@
+x=int(input("Enter any number of your choice:"))
+y=int(input("Enter any number of your choice:"))
+z=int(input("Enter any number of your choice:"))
+a=int(input("Enter any number of your choice:"))
+x=y
+y=z
+z=a
+a=x
