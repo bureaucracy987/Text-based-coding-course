cca220533e20b3b84f99ce16a106c50ece680e05
bureaucracy987 <tulika.de475@gmail.com>
2025-05-26 11:00:24 +0530

    bREAK

---
+++ pyton course/Break/main.py
@@ -0,0 +1,7 @@
+a=input("Enter any word(in capital letters):")
+for i in a:
+    if (i=="A"):
+        print("A has been found in the word",a)
+        break
+    else:
+        print("A has not been found in the word",a)
