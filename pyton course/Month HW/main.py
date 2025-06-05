91ddf664fe3f08c1d77dc8e7edcd793f1c70789c
bureaucracy987 <tulika.de475@gmail.com>
2025-06-05 12:05:51 +0530

    Months HW

---
+++ pyton course/Month HW/main.py
@@ -0,0 +1,25 @@
+for i in range(1,13):
+    if (i==1):
+        print("It is January")
+    if (i==2):
+        print("It is February")
+    if (i==3):
+        print("It is March")
+    if (i==4):
+        print("It is April")
+    if (i==5):
+        print("It is May")
+    if (i==6):
+        print("It is June")
+    if (i==7):
+        print("It is July")
+    if (i==8):
+        print("It is August")
+    if (i==9):
+        print("It is Semtember")
+    if (i==10):
+        print("It is October")
+    if (i==11):
+        print("It is November")
+    if (i==12):
+        print("It is December")
