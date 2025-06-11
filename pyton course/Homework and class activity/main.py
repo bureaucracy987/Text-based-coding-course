7fcb8327402137e38956e79192e3fe62d936d276
bureaucracy987 <tulika.de475@gmail.com>
2025-06-11 09:04:18 +0530

    Play with lists

---
+++ pyton course/Homework and class activity/main.py
@@ -0,0 +1,10 @@
+L=(4,5,6,9,1,10,19)
+count=0
+for i in L:
+    count+=1
+average=count/len(L)
+print("The sum is",count)
+print("The average is",average)
+L.sort
+print("The smallest unit is:",L[0])
+print("The largest unit is:",L[-1])
