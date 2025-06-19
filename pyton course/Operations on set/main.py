4e25d9423b73ed2099892141d19211f134001be4
bureaucracy987 <tulika.de475@gmail.com>
2025-06-19 09:15:56 +0530

    Operations on set

---
+++ pyton course/Operations on set/main.py
@@ -0,0 +1,12 @@
+my_set={1,2,3}
+print(my_set)
+my_set={1.0,"Hello",(1,2,3)}
+print(my_set)
+my_set={1,2,2,3,1,3}
+print(my_set)
+my_set=set([1,2,3])
+print(my_set)
+num_set=set([0,1,2,5,9])
+print("The original set is:",num_set)
+num_set.pop()
+print("Now, the set is:",num_set)
