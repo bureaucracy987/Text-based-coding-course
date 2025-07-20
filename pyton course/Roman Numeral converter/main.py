2d1ee5fdc5b5f9f5a3e03dc4b57529d8377a48f4
bureaucracy987 <tulika.de475@gmail.com>
2025-07-20 11:36:00 +0530

    Roman Numeral converter

---
+++ pyton course/Roman Numeral converter/main.py
@@ -0,0 +1,21 @@
+class RomanConverter:
+    def __init__(self):
+        self.value_map = [
+            (1000, 'M'), (900, 'CM'),
+            (500, 'D'), (400, 'CD'),
+            (100, 'C'), (90, 'XC'),
+            (50, 'L'), (40, 'XL'),
+            (10, 'X'), (9, 'IX'),
+            (5, 'V'), (4, 'IV'),
+            (1, 'I')
+        ]
+
+    def int_to_roman(self, num):
+        roman = ""
+        for value, numeral in self.value_map:
+            while num >= value:
+                roman += numeral
+                num -= value
+        return roman
+converter = RomanConverter()
+print(converter.int_to_roman(1987))
